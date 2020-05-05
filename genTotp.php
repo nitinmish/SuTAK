b3e6b85f2b8c4eb9829e9b1388f43bcbbf5c8673
Nitin Mishra <nitinmish@hotmail.com>
2020-05-05 11:46:34 +0530

    refresh changes to PHP

--- genTotp.php
+++ genTotp.php
@@ -1,4 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="refresh" content="<?php echo 610?>;URL='<?php echo $_SERVER['PHP_SELF'];?>'">
+</head>
 <?
 	$myotp=exec('bash ./genTOpt.bash');
-	printf ("$myotp\n");
+	date_default_timezone_set('Asia/Kolkata');
+	echo ("<body><br>$myotp</br><br>" . date("h:i:sa d-m-Y") . "</br></body>");
 ?>
+
+</html>
